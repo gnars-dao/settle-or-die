5b0dbb34ae6395ef2346c7ad7756aa7ac4feade8
Vlad 2 <116202536+sktbrd@users.noreply.github.com>
2023-06-16 00:27:00 -0300

    initial commit

---
+++ background.js
@@ -0,0 +1,95 @@
+let auctionEnded = false;
+let auctionEndTime = new Date();
+
+function countdown(targetDate) {
+  const now = new Date();
+  let diff = targetDate.getTime() - now.getTime();
+
+  // Ensure that the target date is in the future
+  if (diff < 0) {
+    return "00:00";
+  }
+
+  let hours = Math.floor(diff / (1000 * 60 * 60));
+  diff -= hours * (1000 * 60 * 60);
+  let mins = Math.floor(diff / (1000 * 60));
+
+  hours = hours < 10 ? "0" + hours : hours;
+  mins = mins < 10 ? "0" + mins : mins;
+  const countdownTime = hours + ":" + mins;
+
+  // Update Chrome badge text
+  chrome.action.setBadgeText({ text: countdownTime });
+
+  return countdownTime;
+}
+
+function checkSettle() {
+  // NOUNS QUERY AND URL - Un/Comment to see
+  // const url = 'https://api.thegraph.com/subgraphs/name/nounsdao/nouns-subgraph';
+  // let query = `
+  // {
+  //   auctions(first: 1, orderBy: endTime, orderDirection: desc) {
+  //     settled
+  //     id
+  //     noun {
+  //       id
+  //     }
+  //     endTime
+  //   }
+  // }`;
+
+  // GNARS QUERY AND URL - Un/Comment to see
+  const url = 'https://api.thegraph.com/subgraphs/name/gnarsdao/gnars';
+  let query = `
+  {
+    auctions(first: 1, orderBy: endTime, orderDirection: desc) {
+      settled
+      id
+      gnar {
+        id
+      }
+      endTime
+    }
+  }`;
+
+  // Request options
+  let options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: query
+    }),
+  };
+
+  // Send the request
+  fetch(url, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => data.data.auctions[0])
+    .then(auction => {
+      const { endTime } = auction;
+      const now = new Date();
+      const lastAuctionDate = new Date(Number(`${endTime}000`));
+      auctionEnded = now > lastAuctionDate;
+      auctionEndTime = lastAuctionDate;
+
+      if (auctionEnded) {
+        chrome.action.setBadgeText({ text: `Settle` });
+        return;
+      }
+
+      countdown(auctionEndTime);
+    })
+    .catch(error => console.log('There has been a problem with your fetch operation: ', error));
+}
+
+checkSettle();
+setInterval(checkSettle, 1000 * 30); // Check every 30 seconds
+setInterval(() => countdown(auctionEndTime), 1000); // Update countdown every second
